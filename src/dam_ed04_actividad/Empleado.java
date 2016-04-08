e1391e4ace3a6994fe36e4742accacbcccc9246f
Allxn95 <Alllansin@gmail.com>
2016-04-08 01:31:48 +0000

    Este es el cuarto commit

--- src/dam_ed04_actividad/Empleado.java
+++ src/dam_ed04_actividad/Empleado.java
@@ -10,6 +10,30 @@ package dam_ed04_actividad;
  * @author Allla_000
  */
 public class Empleado {
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
     public String nombre; 
     public String apellidos; 
     public String dni; 
